0bc47fb6bf0064e0a78b5aaafdbac1e5fdf16d3e
krishna <krishnarajkhg@gmail.com>
2020-05-20 23:43:03 +0530

    testing github commit

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 0xf0
+struct aa{
+int data[11];
+};
+int main()
+{
+   char c;
+   struct aa d;
+     c=10; 
+   if(c=='a') 
+    printf("C =%d true...\n",c);
+ else
+  printf(".....vvvvvvvvvvvvvvvv...\n");
+    return 0;
+}
